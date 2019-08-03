09aa5311b67346e4d30d06a7b8edb9e3e3760800
Naice-gif <nuwitonze98@gmail.com>
2019-08-03 18:26:58 +0200

    my j query

---
+++ java/scripts.js
@@ -0,0 +1,9 @@
+function order(size,crust, toppings){
+    this.size =size;
+    this.crust = crust;
+    this.toppings = topping;
+    this.flavours = flavours;
+}
+var s = ["ExtraLarge" , "Large" , "Medium" , "Small" , "ExtraSmall"];
+var c = ["neopolitan","traditionalPan","focaccia", "sourDough", "thinCrust", "crackerCrust", "stuffedDough", "flatBread"];
+var t= ["Pepperoni","Mushrooms","extra cheese","","","","","","",]
