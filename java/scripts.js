bd2bfbfd8768648d63b3b6c00cc459ef2d711760
Naice-gif <nuwitonze98@gmail.com>
2019-08-05 13:17:20 +0200

    javascript changes

--- java/scripts.js
+++ java/scripts.js
@@ -179,3 +179,46 @@ $(function () {
                 };
             }); 
         });
+
+        $("ul#your").append("<li><span class='contact'>" + newPizza_Order.fullOrder() + "</span></li>");
+        $(".contact").last().click(function(){
+           $("#show-your-order").show();
+           $("#show-your-order h5").text(newPizza_Order.fullOrder());
+           $(".pizza-size").text(newPizza_Order.sizePizza);
+           $(".pizza-type").text(newPizza_Order.typePizza);
+           $(".pizza-number").text(newPizza_Order.numberPizza);
+           $(".pizza-favor").text(newPizza_Order.favoritePizza);
+           $(".pizza-local").text(newPizza_Order.locationPizza);
+           // $("ul#addresses").text("");
+           // $(".pizza-local").text(newPizza_Order.locationPizza);
+           // newPizza_Order.addresses.forEach(function(address) {
+           //   $("ul#addresses").append("<li>" + address.numberPizza + ", " + address.favoritePizza+ "</li>");
+           // });
+
+        //$("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+        //     $(".contact").last().click(function() {
+        //       $("#show-contact").show();
+        //       $("#show-contact h2").text(newContact.fullName());
+        //       $(".first-name").text(newContact.firstName);
+        //       $(".last-name").text(newContact.lastName);
+        //       $("ul#addresses").text("");
+        //       newContact.addresses.forEach(function(address) {
+        //         $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
+        //       });
+        //     });
+        let size, email;
+     size = document.getElementById('#size').value;
+     email = document.getElementById('#email').value;
+
+   function subscribe() {
+    alert("Hello " + email + "Thank for reaching out");
+    console.log(email);
+}
+
+let validateInput = () => {
+    if (size === "") {
+        console.log("Please specify the size");
+    }
+}
+
