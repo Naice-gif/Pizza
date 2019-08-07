942edf4f41371f4baf9147f4db1c22849053328f
Naice-gif <nuwitonze98@gmail.com>
2019-08-07 13:41:11 +0200

    somechanges in all the html, css and javascript

--- java/scripts.js
+++ java/scripts.js
@@ -1,98 +1,184 @@
-document.getElementById("").addEventListener("click", function () {
-    prompt("enter your address");
-    alert("Thank you for considering us, delivery shall be there in few"); 
-   })
-  
-  document.getElementById("pick").addEventListener("click", function () {
-    alert("Thank you for visiting us!"); 
-   })
-  function Pizza(size,crust,topping,quantity,total){
-      this.size=size;
-      this.crust=crust;
-      this.topping=topping;
-      this.quantity=quantity;
-      this.total= total
-  };
-  // Pizza.prototype.getPrice=function(){
-  //   return this.totalPrice
-  
-  $(document).ready(function(){ 
-      $("#brief").click(function(event){
-      event.preventDefault();
-      var selectSize=$("#size option:selected").text();
-      var selectCrust=$("crust option:selected").text();
-      var selectTopping=$("#toppings option:selected").text();
-      var inputQuantity=$("input#quantity").val();
-         
-     
-      var pizzaSize=$("#size").val(); 
-      var pizzaCrust=$("#crust").val();
-      var pizzaTops=$("#toppings").val();
-      var pizzaQuantity=$("#quantity").val();
-      var totalPrice
-           
-      Pizza.prototype.getPrice=function(){
-        return this.totalPrice};
-      // console.log(newPizza);
-         
-  
-        
-        
-  ////////////////////////pricing codes /////////////////
-  
-  let price;
-  
-        switch (pizzaSize) {
-            case pizzaSize = "regular":
-                price = 1000;
-                if (pizzaCrust === "thin" && pizzaTops===$("#toppings").val()){
-                  totalPrice = (price * pizzaQuantity) + 1000;
-                } else if (pizzaCrust === "thick"&& pizzaTops===$("#toppings").val()) {
-                  totalPrice = (price * pizzaQuantity) + 1500;
-                } else if (pizzaCrust === "flatbread"&& pizzaTops===$("#toppings").val()) {
-                  totalPrice = (price * pizzaQuantity) + 2000;
-                }else if (pizzaCrust==="custom"&& pizzaTops===$("#toppings").val()){
-                  totalPrice = (price*pizzaQuantity)+ 2500
-                }
-                
-                break;
-            case pizzaSize = "medium":
-              price=2000
-              if (pizzaCrust === "thin" && pizzaTops===$("#toppings").val()){
-                totalPrice = (price * pizzaQuantity) + 2000;
-            } else if (pizzaCrust === "thick"&& pizzaTops===$("#toppings").val()) {
-              totalPrice = (price * pizzaQuantity) + 2500;
-            } else if (pizzaCrust === "flatbread"&& pizzaTops===$("#toppings").val()) {
-              totalPrice = (price * pizzaQuantity) + 3000;
-            }else if (pizzaCrust==="custom"&& pizzaTops===$("#toppings").val()){
-              totalPrice = (price*pizzaQuantity)+ 3500
-             } 
+var Orders = function(crust,toppings,size,number){
+  this.crust = crust;
+  this.toppings = toppings;
+  this.size = size;
+  this.number = number;
+}
 
-                break;
-            case pizzaSize = "large":
-                price = 3000;
-                if (pizzaCrust === "thin" && pizzaTops===$("#toppings").val()){
-                  totalPrice = (price * pizzaQuantity) + 3000;
-              } else if (pizzaCrust === "thick"&& pizzaTops===$("#toppings").val()) {
-                totalPrice = (price * pizzaQuantity) + 3500;
-              } else if (pizzaCrust === "flatbread"&& pizzaTops===$("#toppings").val()) {
-                totalPrice = (price * pizzaQuantity) + 4000;
-              }else if (pizzaCrust==="custom"&& pizzaTops===$("#toppings").val()){
-                totalPrice = (price*pizzaQuantity)+ 4500
-              }
-            }
-            var newPizza = new Pizza(pizzaSize,pizzaCrust,pizzaTops,pizzaQuantity,totalPrice);
-  
-              
-              console.log(newPizza)
-              console.log(totalPrice)
-              $(".list").show();
-              $(".size").text(newPizza.size);
-              $(".type").text(newPizza.crust);
-              $(".your").text(newPizza.topping);
-              $(".number").text(newPizza.quantity);
-              $(".ttt").text(newPizza.total);  
-  
-          })
-          });
+Orders.prototype.fullOrder = function (){
+  return this.number + " " + this.size + ", " +  this.crust +", " + this.toppings;
+}
+var topArray = ["Sausage, caramelized onions,fennel", "Sardines,red onions,black olives" , "BBQ sauce, grilled chicken" , "corned beef pastrami,Swiss cheese" , "Corn,spicy Italian salami", "Buffalo chicken,blue cheese"];
+var sizePrice,crustPrice,toppingsPrice;
+var totalPrice = [];
+var sumTotal = 0;
+function price(size,crust,toppings){ 
+  if(size==="Regular"){
+      sizePrice = 10;
+      if(crust==="thin crust"){
+          crustPrice = 2;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="thick crust" || crust=== "FlatBread crust"){
+          crustPrice=4;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="Custom crust"){
+          crustPrice=3;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+  }
+  else if(size==="Medium"){
+      sizePrice = 15;
+      if(crust==="thin crust"){
+          crustPrice = 2;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="thick crust" || crust==="FlatBread crust"){
+          crustPrice=4;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="Custom crust"){
+          crustPrice=3;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+  }
+  else if(sizePrice==="Large"){
+      sizePrice = 20;
+      if(crust==="thin crust"){
+          crustPrice = 2;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===toppingsArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="thick crust" || crust==="FlatBread crust"){
+          crustPrice=4;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+      else if(crust==="Custom crust"){
+          crustPrice=3;
+          if (toppings===topArray[0] || toppings===topArray[1]){
+              toppingsPrice = 10;
+          }
+          else if(toppings===topArray[2] || toppings===topArray[4]){
+              toppingsPrice = 15;
+          }
+          else if(toppings===topArray[3] || toppings===topArray[5]){
+              toppingsPrice = 20;
+          }
+      }
+  }
+
+  var pricing = ( crustPrice + toppingsPrice + sizePrice ) * number;
+  console.log("pricing: "+pricing);
+  totalPrice.push(pricing);
+  console.log(totalPrice);
+  if(totalPrice.length>0){
+      for(var i=0;i<totalPrice.length;i++){
+          sumTotal+=totalPrice[i];
+          console.log("sum " + sumTotal);
+      }
+  }
+  return (crustPrice + toppingsPrice + sizePrice)  * number;
+}
+
+function resetInputs(){
+  $("#crust").val("");
+  $("#toppings").val("");
+  $("#size").val("");
+  $("#num").val("");
+}
+
+var pizzaCrust, pizzaToppings, pizzaSize, number;
+$(document).ready(function(){
+  $("#lookup").click(function(event){
+      event.preventDefault();
+      alert("Please enter a new order or checkup what you order");
+      
+      pizzaCrust = $("#crust").val();
+      console.log(pizzaCrust);
+      pizzaToppings = $("#toppings").val();
+      console.log(pizzaToppings);
+      pizzaSize = $("#size").val();
+      console.log(pizzaSize);
+      number = $("#num").val();
+      console.log(number);
+      
+      var newOrder = new Orders(pizzaCrust,pizzaToppings, pizzaSize, number);
+
+      $("ol#orders").append('<li><span>'+ newOrder.fullOrder() +'</span></li>');
+      $("ol#costs").append('<li><span>'+ "cost: " + price(pizzaSize, pizzaCrust, pizzaToppings) + "$" +'</span></li>');
+      resetInputs();
+  });
+
+  $("#checkout").click(function(){
+      $(".hidden").show();
+      $("h4#totals").append(" "+ sumTotal + "$.");
+  });
+
+  });
+ 
+
   
